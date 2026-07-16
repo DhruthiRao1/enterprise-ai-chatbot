b60be11ad700a2e987a7e6f71dec7cf56d7b42ed
DhruthiRao1 <gadhru1@gmail.com>
2026-07-16 10:31:07 +0530

    Create llm.py

---
+++ app/llm.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+def ask_llm(question):
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    )
+
+    return response.choices[0].message.content
